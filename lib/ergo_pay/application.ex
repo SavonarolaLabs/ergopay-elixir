d6db9bf3eabdbb61d6c5fb856f0826783aa6b426
c8e4 <c8e4@proton.me>
2025-07-05 19:53:29 +0200

    add prod certs

--- lib/ergo_pay/application.ex
+++ lib/ergo_pay/application.ex
@@ -1,13 +1,23 @@
-defmodule ErgoPay.Application do
-  use Application
+def start(_, _) do
+  children = [
+    ErgoPay.Session,
+    {Plug.Cowboy, scheme: scheme(), plug: ErgoPay.Router, options: server_options()}
+  ]
 
-  def start(_type, _args) do
-    children = [
-      ErgoPay.Session,
-      {Plug.Cowboy,
-       scheme: :http, plug: ErgoPay.Router, options: [port: Application.get_env(:ergo_pay, :port)]}
-    ]
+  Supervisor.start_link(children, strategy: :one_for_one, name: ErgoPay.Supervisor)
+end
 
-    Supervisor.start_link(children, strategy: :one_for_one, name: ErgoPay.Supervisor)
+defp scheme, do: if(Mix.env() == :prod, do: :https, else: :http)
+
+defp server_options do
+  port = String.to_integer(System.get_env("PORT", "8443"))
+  base = [port: port]
+
+  if Mix.env() == :prod do
+    key = System.get_env("SSL_KEY", "/etc/letsencrypt/live/crystalpool.cc/privkey.pem")
+    cert = System.get_env("SSL_CERT", "/etc/letsencrypt/live/crystalpool.cc/fullchain.pem")
+    Keyword.merge(base, keyfile: key, certfile: cert)
+  else
+    base
   end
 end
