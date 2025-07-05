5dbf5c7e06bbf17a5aafaf154134407d7f582480
c8e4 <c8e4@proton.me>
2025-07-05 19:56:47 +0200

    fix app

--- lib/ergo_pay/application.ex
+++ lib/ergo_pay/application.ex
@@ -1,23 +1,30 @@
-def start(_, _) do
-  children = [
-    ErgoPay.Session,
-    {Plug.Cowboy, scheme: scheme(), plug: ErgoPay.Router, options: server_options()}
-  ]
+defmodule ErgoPay.Application do
+  @moduledoc false
+  use Application
 
-  Supervisor.start_link(children, strategy: :one_for_one, name: ErgoPay.Supervisor)
-end
+  def start(_type, _args) do
+    children = [
+      ErgoPay.Session,
+      {Plug.Cowboy, scheme: scheme(), plug: ErgoPay.Router, options: server_options()}
+    ]
 
-defp scheme, do: if(Mix.env() == :prod, do: :https, else: :http)
+    Supervisor.start_link(children, strategy: :one_for_one, name: ErgoPay.Supervisor)
+  end
 
-defp server_options do
-  port = String.to_integer(System.get_env("PORT", "8443"))
-  base = [port: port]
+  defp scheme do
+    if Mix.env() == :prod, do: :https, else: :http
+  end
 
-  if Mix.env() == :prod do
-    key = System.get_env("SSL_KEY", "/etc/letsencrypt/live/crystalpool.cc/privkey.pem")
-    cert = System.get_env("SSL_CERT", "/etc/letsencrypt/live/crystalpool.cc/fullchain.pem")
-    Keyword.merge(base, keyfile: key, certfile: cert)
-  else
-    base
+  defp server_options do
+    port = String.to_integer(System.get_env("PORT", "8443"))
+    base_opts = [port: port]
+
+    if Mix.env() == :prod do
+      keyfile = System.get_env("SSL_KEY", "/etc/letsencrypt/live/crystalpool.cc/privkey.pem")
+      certfile = System.get_env("SSL_CERT", "/etc/letsencrypt/live/crystalpool.cc/fullchain.pem")
+      Keyword.merge(base_opts, keyfile: keyfile, certfile: certfile)
+    else
+      base_opts
+    end
   end
 end
