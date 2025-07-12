a19b30261527dcbf495fa6aebf8598aedf7e9111
c8e4 <c8e4@proton.me>
2025-07-12 19:48:39 +0200

    add address log

--- lib/ergo_pay/router/auth.ex
+++ lib/ergo_pay/router/auth.ex
@@ -6,11 +6,16 @@ defmodule ErgoPay.Router.Auth do
     id = conn.path_params["id"] || conn.params["id"]
     addr = conn.path_params["address"] || conn.params["address"]
 
+    # Log incoming address info
+    IO.puts("[AUTH REQUEST] id=#{inspect(id)} address=#{inspect(addr)} method=#{conn.method}")
+
     cond do
       id == nil ->
+        log_response(nil, "Missing id", "ERROR")
         json(conn, 400, %{address: nil, message: "Missing id", messageSeverity: "ERROR"})
 
       addr == "multiple_check" ->
+        log_response(nil, "multiple addresses supported", "INFORMATION")
         json(conn, 200, %{
           address: nil,
           message: "multiple addresses supported",
@@ -24,6 +29,7 @@ defmodule ErgoPay.Router.Auth do
           is_list(list) and list != [] ->
             first = hd(list)
             ErgoPay.Session.set(id, %{list: list, addr: first})
+            log_response(first, "address list stored", "INFORMATION")
 
             json(conn, 200, %{
               address: first,
@@ -32,6 +38,7 @@ defmodule ErgoPay.Router.Auth do
             })
 
           true ->
+            log_response(nil, "Invalid address list", "ERROR")
             json(conn, 400, %{
               address: nil,
               message: "Invalid address list",
@@ -41,6 +48,7 @@ defmodule ErgoPay.Router.Auth do
 
       is_binary(addr) ->
         ErgoPay.Session.set(id, %{addr: addr})
+        log_response(addr, "address received", "INFORMATION")
 
         json(conn, 200, %{
           address: addr,
@@ -51,6 +59,7 @@ defmodule ErgoPay.Router.Auth do
       true ->
         case ErgoPay.Session.get(id) do
           nil ->
+            log_response(nil, "Not connected", "ERROR")
             json(conn, 400, %{
               address: nil,
               message: "Not connected",
@@ -58,6 +67,7 @@ defmodule ErgoPay.Router.Auth do
             })
 
           %{addr: a} ->
+            log_response(a, "connected", "INFORMATION")
             json(conn, 200, %{
               address: a,
               message: "connected",
@@ -67,7 +77,6 @@ defmodule ErgoPay.Router.Auth do
     end
   end
 
-  # Plug wraps root-level JSON arrays in %{"_json" => list}
   defp extract_list(%{"_json" => l}) when is_list(l), do: l
   defp extract_list(l) when is_list(l), do: l
   defp extract_list(_), do: nil
@@ -77,5 +86,9 @@ defmodule ErgoPay.Router.Auth do
     |> put_resp_content_type("application/json")
     |> send_resp(status, Jason.encode!(map))
     |> halt()
+  end
+
+  defp log_response(addr, message, severity) do
+    IO.puts("[AUTH RESPONSE] address=#{inspect(addr)} message=#{message} severity=#{severity}")
   end
 end
